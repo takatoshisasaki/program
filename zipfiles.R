e132994e76358767137ebaf3a0c865c68930e95f
takatoshisasaki <44063074+takatoshisasaki@users.noreply.github.com>
2026-01-03 16:27:28 +0900

    Add files via upload

---
+++ zipfiles.R
@@ -0,0 +1,55 @@
+
+rm(list = ls()); gc()
+library(dplyr)
+library(stringr)
+setwd("C:/Users/takat/Desktop/test/rawdata")
+
+# utility
+rawdatalist <- list.files()
+filelist_year <- as.data.frame(table(str_sub(rawdatalist, 2, 3)))
+yearseq <- unique(filelist_year$Var1)[]
+monthseq1 <- c("01", "02", "03")
+monthseq2 <- c("04", "05", "06")
+monthseq3 <- c("07", "08", "09")
+monthseq4 <- c("10", "11", "12")
+
+#### zip ####
+specific_ym1 <- rawdatalist[str_sub(rawdatalist, 2, 3) == "02"]
+zip("b02.zip", specific_ym1)
+
+for(i in 2:length(yearseq)){
+  specific_ym1 <- rawdatalist[str_sub(rawdatalist, 2, 3) == yearseq[i] & str_sub(rawdatalist, 4, 5) %in% monthseq1]
+  specific_ym2 <- rawdatalist[str_sub(rawdatalist, 2, 3) == yearseq[i] & str_sub(rawdatalist, 4, 5) %in% monthseq2]
+  specific_ym3 <- rawdatalist[str_sub(rawdatalist, 2, 3) == yearseq[i] & str_sub(rawdatalist, 4, 5) %in% monthseq3]
+  specific_ym4 <- rawdatalist[str_sub(rawdatalist, 2, 3) == yearseq[i] & str_sub(rawdatalist, 4, 5) %in% monthseq4]
+  
+  name1 <- paste0("b", yearseq[i], "_1Q.zip")
+  name2 <- paste0("b", yearseq[i], "_2Q.zip")
+  name3 <- paste0("b", yearseq[i], "_3Q.zip")
+  name4 <- paste0("b", yearseq[i], "_4Q.zip")
+  
+  zip(name1, specific_ym1)
+  print(name1)
+  zip(name2, specific_ym2)
+  print(name2)
+  zip(name3, specific_ym3)
+  print(name3)
+  zip(name4, specific_ym4)
+  print(name4)
+}
+
+#### Unzip ####
+setwd("C:/Users/takat/Desktop/test2")
+unzip("b02.zip")
+for(i in 2:length(yearseq)){
+  name1 <- paste0("b", yearseq[i], "_1Q.zip")
+  name2 <- paste0("b", yearseq[i], "_2Q.zip")
+  name3 <- paste0("b", yearseq[i], "_3Q.zip")
+  name4 <- paste0("b", yearseq[i], "_4Q.zip")
+  
+  unzip(name1); print(name1)
+  unzip(name2); print(name2)
+  unzip(name3); print(name3)
+  unzip(name4); print(name4)
+}
+
